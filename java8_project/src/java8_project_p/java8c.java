a9cf20555ba1db7a7a0560f0f706cb0258158814
okamotosyuto <shootup.encourage@gmail.com>
2023-05-23 12:25:16 +0900

    java 1-8 修正

--- java8_project/src/java8_project_p/java8c.java
+++ java8_project/src/java8_project_p/java8c.java
@@ -3,170 +3,196 @@ package java8_project_p;
 public class java8c {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
 
-//Q1
-byte a;
-short b;
-int c;
-long d;
-float e;
-double f;
-char g;
-String h;
-boolean i;
+		//Q1
+		byte a;
 
+		short b;
 
-//Q2
-a = 0;
-b = 0;
-c = 0;
-d = 0;
-e = 0f;
-f = 0.0d;
-g = '\u0000';
-h = null;
-i = true;
+		int c;
 
+		long d;
 
-//Q3
-a = 10;
-b = 100;
-c = 1000;
-d = 10000;
-e = 9.5f;
-f = 10.5d;
-g = 'a';
-h = "ハロー";
-i = true;
+		float e;
 
+		double f;
 
-//Q4
-long Q1 = a + b + c + d;
-System.out.println(Q1);
+		char g;
 
-double Q2 = e + f;
-System.out.println(Q2);
+		String h;
 
-System.out.println(g + " " + h + " " + i);
+		boolean i;
 
-double Q3 = Q1 + Q2;
-System.out.println(Q3);
+		//Q2
+		a = 0;
 
-long Q4 = a * b * c * d;
-System.out.println(Q4);
+		b = 0;
 
-double Q5 = f / 100;
-System.out.println(Q5);
+		c = 0;
 
-int Q6 = a - b;
-System.out.println(Q6);
+		d = 0;
+
+		e = 0f;
+
+		f = 0.0d;
+
+		g = '\u0000';
+
+		h = null;
+
+		i = false;
 
 
-//Q5
-String num = "20";
-String num1 = "23";
-System.out.println("ハローJAVA" + (num + num1));
+
+		//Q3
+		a = 10;
+
+		b = 100;
+
+		c = 1000;
+
+		d = 10000;
+
+		e = 9.5f;
+
+		f = 10.5d;
+
+		g = 'a';
+
+		h = "ハロー";
+
+		i = false;
 
 
-//Q6
-String name = "山田太郎";
-System.out.println("初めまして" + name + "です");
+		//Q4
+		long Q1 = a + b + c + d;
+		System.out.println(Q1);
 
-int age = 18;
-System.out.println("年齢は" + age + "歳です");
+		double Q2 = e + f;
+		System.out.println(Q2);
 
-double height = 170.5;
-System.out.println("身長は" + height + "cmです");
+		System.out.println(g + " " + h + " " + i);
 
-double weight = 62.2;
-System.out.println("体重は" + weight + "kgです");
+		double Q3 = Q1 + Q2;
+		System.out.println(Q3);
 
-String favorite = "寿司";
-System.out.println("好きな食べ物は" + favorite + "です");
+		long Q4 = a * b * c * d;
+		System.out.println(Q4);
 
-//Q7
-double bmi =weight / Math.pow(height / 100, 2);
-System.out.println("BMIは" + bmi + "です");
+		double Q5 = f / 100;
+		System.out.println(Q5);
 
-
-//Q8
-name = "鈴木一郎";
-System.out.println("初めまして" + name + "です");
-
-age = 24;
-System.out.println("年齢は" + age + "歳です");
-
-height = 168.5;
-System.out.println("身長は" + height + "cmです");
-
-weight = 64.2;
-System.out.println("体重は" + weight + "kgです");
-
-favorite = "オムライス";
-System.out.println("好きな食べ物は" + favorite + "です");
-
-double bmi2 =weight / Math.pow(height / 100, 2);
-System.out.println("BMIは" + bmi + "です");
+		int Q6 = a - b;
+		System.out.println(Q6);
 
 
-//Q9
-name = "鈴木一郎";
-System.out.println("初めまして" + name + "です");
+		//Q5
+		String num = "20";
 
-age = age + age;
-System.out.println("年齢は" + age + "歳です");
+		String num1 = "23";
 
-height = height + height;
-System.out.println("身長は" + height + "cmです");
-
-weight = weight + weight;
-System.out.println("体重は" + weight + "kgです");
-
-favorite = "オムライス";
-System.out.println("好きな食べ物は" + favorite + "です");
-
-double bmi3 =weight / Math.pow(height / 100, 2);
-System.out.println("BMIは" + bmi + "です");
+		System.out.println("ハローJAVA" + (num + num1));
 
 
-//Q10
-age = 24;
-height = 168.5;
-weight = 64.2;
-if (age >= 25){
-	System.out.println("true");
-}
+		//Q6
+		String name = "山田太郎";
+		System.out.println("初めまして" + name + "です");
+
+		int age = 18;
+		System.out.println("年齢は" + age + "歳です");
+
+		double height = 170.5;
+		System.out.println("身長は" + height + "cmです");
+
+		double weight = 62.2;
+		System.out.println("体重は" + weight + "kgです");
+
+		String favorite = "寿司";
+		System.out.println("好きな食べ物は" + favorite + "です");
+
+		//Q7
+		double bmi =weight / Math.pow(height / 100, 2);
+		bmi = (double)Math.round(bmi *10)/10;
+		System.out.println("BMIは" + bmi + "です");
 
 
-//Q11
-String age2 = String.valueOf(age);
-System.out.println(age2);
+		//Q8
+		name = "鈴木一郎";
+		System.out.println("初めまして" + name + "です");
 
-String height2 = String.valueOf(height);
-System.out.println(height2);
+		age = 24;
+		System.out.println("年齢は" + age + "歳です");
 
-String weight2 = String.valueOf(weight);
-System.out.println(weight2);
+		height = 168.5;
+		System.out.println("身長は" + height + "cmです");
+
+		weight = 64.2;
+		System.out.println("体重は" + weight + "kgです");
+
+		favorite = "オムライス";
+		System.out.println("好きな食べ物は" + favorite + "です");
+
+		double bmi2 =weight / Math.pow(height / 100, 2);
+		bmi2 = (double)Math.round(bmi2 *10)/10;
+		System.out.println("BMIは" + bmi2 + "です");
 
 
-//Q12
-int age3 = Integer.valueOf(age2);
-System.out.println(age3);
+		//Q9
+		name = "鈴木一郎";
+		System.out.println("初めまして" + name + "です");
 
-double height3 = Double.parseDouble(height2);
-//System.out.println(Math.round(height3));
-int height4 = (int)height3;
-System.out.println(height4);
+		age = age + age;
+		System.out.println("年齢は" + age + "歳です");
+
+		height = height + height;
+		System.out.println("身長は" + height + "cmです");
+
+		weight = weight + weight;
+		System.out.println("体重は" + weight + "kgです");
+
+		favorite = "オムライス";
+		System.out.println("好きな食べ物は" + favorite + "です");
+
+		double bmi3 =weight / Math.pow(height / 100, 2);
+		bmi3 = (double)Math.round(bmi3 *10)/10;
+		System.out.println("BMIは" + bmi3 + "です");
 
 
-//Q13
-int age5 = 24;
-int height5 = 159;
-boolean flg = age5 >= 25 ?
-true : height5 >= 160 ? true:false;
-System.out.println(flg);
-	
-}
+		//Q10
+		age = 24;
+
+		height = 168.5;
+
+		weight = 64.2;
+		boolean flg = age >= 25 ? true : false;
+		System.out.println(flg);
+
+
+		//Q11
+		String age2 = String.valueOf(age);
+		System.out.println(age2);
+
+		String height2 = String.valueOf(height);
+		System.out.println(height2);
+
+		String weight2 = String.valueOf(weight);
+		System.out.println(weight2);
+
+
+		//Q12
+		int age3 = Integer.valueOf(age2);
+		System.out.println(age3);
+
+		double height3 = Double.parseDouble(height2);
+		// System.out.println(Math.round(height3));
+		int height4 = (int)height3;
+		System.out.println(height4);
+
+
+		//Q13
+		boolean flg2 = age3 >= 25 ? true : height4 >= 160 ? true:false;
+		System.out.println(flg2);
+
+	}
 }
 
